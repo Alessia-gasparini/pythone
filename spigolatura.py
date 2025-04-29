40209ee65acdfacfb5481b775e7d293d48d23acb
Alessia Gasparini <al3.gasparini@gmail.com>
2025-04-29 11:27:12 +0200

    Add files via upload

---
+++ spigolatura.py
@@ -0,0 +1,9 @@
+import urllib.request
+url = "https://www.cmaantincendio.it/"
+risultato = urllib.request.urlopen(url)
+theBytes = risultato.read()
+text = theBytes.decode()
+
+import bs4
+doc = bs4.BeautifulSoup(text)
+print(doc.prettify())
