12bf6a11412b089611b470043f5a020a0294f41d
Alessia Gasparini <al3.gasparini@gmail.com>
2025-04-30 15:25:07 +0200

    Add files via upload

---
+++ ripasso.py
@@ -0,0 +1,62 @@
+# funzioni built-in gia disponibili -> type(5) ----> print(type)
+
+# funzioni di libreria da importare -> sqrt(5)
+
+# metodi -> funzione associata ad un oggetto
+# funzioni -> blocco di codice indipendente
+# string.strartswithz -> il . serve per invocare un metodo in self
+# 
+# moltiplica la strainga -> print(5 * "a") 
+# non si possono moltiplicare le stringhe -> print("a"* "b") 
+
+# conversioni numeri in stringhe e viceversa
+
+# operatore di formato -> % formattazione prima ora f"stringa(unisce più valori)
+
+# input utente -> qualsiasi tipo di dato
+# input numerico -> un dato gia specificato
+
+# : termina un istruzone
+# operatori booleani -> and() or() not()
+
+# ciclo while-> continuo finche...
+# cicli -> ripetizioni
+# for
+# annidati
+
+# funzioni -> def
+# passaggi di parametri
+# ambito di visibilità di variabile -> singola o conteir
+
+# liste
+# ->letura
+# ->accesso
+# ->aggiunta
+# ->creazione
+# ->scansione
+# ->eliminazione
+
+# eccezioni -> come sollecitarla e come gestirla -> try -> singole except -> clausola finally
+# gestione del file
+# incapsulamento -> consente di nascondere i dettagli di implementazione di una classe e di proteggere i dati da accessi non autorizzati (_)
+
+# costruttore -> unico -> __init__ (nell'oop costruttori di classe possono essere multipli si differenziano tramite la firma)
+# varianile di istanza -> dentro ad un metodo di una classe -> sintassi (self.) spazio di memoria per le variabili
+
+# ereditarietà -> possibilità di creare delle classi derivando da una classe piu semplice gia esistente, una più complessa memorizzando gli attributi
+# class (nome) deriva da (nome ereditato)
+# super
+
+# sovvrascrittura di un metodo -> avviene quando una classe derivata definisce un metodo con lo stesso nome di un metodo nella sua classe base (superclasse).
+
+# urllib per l'accesso al web
+# il formato html BeautifulSoup
+# web Scraping
+
+#Lamda programmazione funzionale (si passa una funzione)
+x = lambda a : a + 10
+print(x(5))
+
+# -> sorted function ordina gli elementi per riferirne una nuova
+
+# per vedere i metodi di una funzione (dir)
